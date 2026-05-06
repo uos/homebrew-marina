7c31a3b2be091e41186b66d079ee5b308af5d854
stelzo <stelzo@steado.de>
2026-05-06 09:14:43 +0000

    marina 0.2.5

--- Formula/marina.rb
+++ Formula/marina.rb
@@ -1,16 +1,16 @@
 # Maintainer: Christopher Sieh (stelzo) <stelzo@steado.de>
-# Template file — CI replaces 0.2.4 and e3fbc5546b85e60b1887a672d500f133e4b1b2b3403aadc726a4515dd87d9dac
+# Template file — CI replaces 0.2.5 and be8b3676889ee41c7b912d3e280bfad85532d9fe71d9848413b7a9249494a20a
 # before publishing to the tap.
 class Marina < Formula
   desc "A dataset manager for robotics to organize, share, and discover datasets and metadata across storage backends."
   homepage "https://codeberg.org/stelzo/marina"
-  version "0.2.4"
+  version "0.2.5"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     on_arm do
       url "https://uos-robotics.codeberg.page/ppa/bin/marina-#{version}-macos-arm64.tar.gz"
-      sha256 "e3fbc5546b85e60b1887a672d500f133e4b1b2b3403aadc726a4515dd87d9dac"
+      sha256 "be8b3676889ee41c7b912d3e280bfad85532d9fe71d9848413b7a9249494a20a"
     end
   end
 
